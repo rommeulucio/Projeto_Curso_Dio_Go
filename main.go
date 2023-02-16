b0e70b23ebd1e93388b06bc2f4c1a903b7981585
rommeulucio <rommeulucio@gmail.com>
2023-02-16 08:12:51 -0300

    update curso_dio_go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome = "José"
+	var idade = 50
+	var versao = 1.8
+	fmt.Println("Oi SR.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+}
